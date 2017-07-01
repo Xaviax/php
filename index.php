ad02141c27266fbe211d5c3129a9f7037beef535
Xaviax <lordofdivines@gmail.com>
2017-07-01 16:52:40 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,6 @@
 <?php
 
-<<<<<<< HEAD
 echo "Ola <strong>mundo!</strong> 123";
-=======
-echo "Ola <strong>mundo 123!</strong>";
-
-echo "Ola <strong>Mundo!</strong>";
->>>>>>> 97d34e0f81834a586017621f7f39da10b5f6d2a8
 
 echo "Ola <strong>mundo!</strong> haha";
 ?>
